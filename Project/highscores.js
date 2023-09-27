7d739c9f8076d77029542c93bfd959020435c80e
Jackson Neuman <jacksonneuman@Jacksons-Air-4.attlocal.net>
2023-09-26 21:37:04 -0500

    updated all my files

--- Project/highscores.js
+++ Project/highscores.js
@@ -1,45 +1,24 @@
 // Function to retrieve high scores from local storage
 function getHighScores() {
-    // Check if high scores exist in local storage
     const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-  
-    // Sort high scores in descending order (highest score first)
-    highScores.sort((a, b) => b.score - a.score);
-  
     return highScores;
-  }
-  
-  // Function to display high scores in the HTML
-  function displayHighScores() {
-    const highScoresList = document.getElementById('high-scores-list');
-    highScoresList.innerHTML = ''; // Clear previous content
-  
-    // Retrieve high scores from local storage
-    const highScores = getHighScores();
-  
-    // Populate the high scores list
-    highScores.forEach((scoreData, index) => {
-      const listItem = document.createElement('li');
-      listItem.textContent = `Player: ${scoreData.initials} - Score: ${scoreData.score}`;
-      highScoresList.appendChild(listItem);
-    });
-  }
-  
-  
-  // Call the function to display high scores when the page loads
-  window.addEventListener('load', displayHighScores);
+}
 
-  function saveHighScore(initials, score) {
+// Function to display high scores in the HTML
+function displayHighScores() {
+    const highScoresList = document.getElementById('high-scores-list');
+    highScoresList.innerHTML = '';
+
     const highScores = getHighScores();
-  
-    // Create a new high score entry
-    const newHighScore = { initials, score };
-  
-    // Add the new entry to the high scores array
-    highScores.push(newHighScore);
-  
-    // Save the updated high scores array to local storage
-    localStorage.setItem('highScores', JSON.stringify(highScores));
-  }
-  
+
+    highScores.forEach((scoreData, index) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Player: ${scoreData.initials} - Score: ${scoreData.score}`;
+        highScoresList.appendChild(listItem);
+    });
+}
+
+// Call the function to display high scores when the page loads
+window.addEventListener('load', displayHighScores);
+
   
--- Project/quiz.js
+++ Project/quiz.js
@@ -21,113 +21,99 @@ var questions = [
 let currentQuestionIndex = 0;
 let score = 0;
 let timer = 60; // Initial timer value in seconds
-const timerElement = document.getElementById('timer'); // Add an HTML element to display the timer
-const feedbackElement = document.getElementById('feedback'); // Add an HTML element to display feedback
+const timerElement = document.getElementById('timer');
+const feedbackElement = document.getElementById('feedback');
+const initialsForm = document.getElementById('initials-form');
+const initialsInput = document.getElementById('initials');
+const finalScoreElement = document.getElementById('final-score');
 
-let interval; // Declare the interval variable outside the startQuiz function
+let interval;
 
 // Function to start the quiz
 function startQuiz() {
-  // Start the timer countdown
-  interval = setInterval(function () {
-    timerElement.textContent = `Time: ${timer}`;
-    timer--;
+    interval = setInterval(function () {
+        timerElement.textContent = `Time: ${timer}`;
+        timer--;
 
-    if (timer < 0 || currentQuestionIndex === questions.length) {
-      clearInterval(interval);
-      // Handle quiz completion and user input here
-      handleQuizCompletion();
-    }
-  }, 1000);
+        if (timer < 0 || currentQuestionIndex === questions.length) {
+            clearInterval(interval);
+            handleQuizCompletion();
+        }
+    }, 1000);
 
-  // Display the first question
-  displayQuestion(currentQuestionIndex);
+    displayQuestion(currentQuestionIndex);
 }
 
 // Function to handle quiz completion
 function handleQuizCompletion() {
-    // ... (previous code)
-  
-    // Allow user input for initials and save data to local storage
-    const initialsForm = document.getElementById('initials-form');
-    const initialsInput = document.getElementById('initials');
-  
-    // Show the initials form to the user
-    initialsForm.style.display = 'block';
-  
-    // Handle form submission
+    const questionContainer = document.getElementById('question-container');
+    questionContainer.innerHTML = '';
+
+    if (timer <= 0) {
+        feedbackElement.textContent = 'Time is up!';
+    } else {
+        feedbackElement.textContent = 'Quiz completed!';
+    }
+
     initialsForm.addEventListener('submit', function (event) {
-      event.preventDefault(); // Prevent the form from submitting the traditional way
-  
-      const userInitials = initialsInput.value.trim(); // Get and clean the user's initials
-  
-      if (userInitials !== '') {
-        // Save the user's score and initials to local storage
-        saveHighScore(userInitials, score);
-  
-        // Redirect to the high scores page (You can implement this)
-        // For example, you can use window.location.href to redirect:
-        // window.location.href = 'highscores.html';
-      } else {
-        // Provide feedback to the user that initials are required
-        // You can display an error message or add a CSS class to style it
-      }
+        event.preventDefault();
+        const userInitials = initialsInput.value;
+        // Save user's score and initials to local storage
+        const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+        highScores.push({ initials: userInitials, score: score });
+        localStorage.setItem('highScores', JSON.stringify(highScores));
+        // Redirect to high scores page
+        window.location.href = 'highscores.html'; // Change this to the correct path of your highscores.html
     });
-  }
-  
+
+    finalScoreElement.textContent = `Your score: ${score}`;
+}
+
 
 // Function to display a question
 function displayQuestion(index) {
-  feedbackElement.textContent = ''; // Clear previous feedback
-  const questionContainer = document.getElementById('question-container'); // Add a container for the question
-  questionContainer.innerHTML = ''; // Clear previous content
+    feedbackElement.textContent = '';
+    const questionContainer = document.getElementById('question-container');
+    questionContainer.innerHTML = '';
 
-  // Create and display the question
-  const question = document.createElement('h1');
-  question.textContent = questions[index].question;
-  questionContainer.appendChild(question);
+    const question = document.createElement('h1');
+    question.textContent = questions[index].question;
+    questionContainer.appendChild(question);
 
-  // Create and display answer choices as buttons
-  for (let i = 0; i < questions[index].choices.length; i++) {
-    const choiceButton = document.createElement('button');
-    choiceButton.textContent = questions[index].choices[i];
-    // Add event listener to handle user's choice
-    choiceButton.addEventListener('click', function () {
-      handleAnswerClick(this.textContent);
-    });
-    questionContainer.appendChild(choiceButton);
-  }
+    for (let i = 0; i < questions[index].choices.length; i++) {
+        const choiceButton = document.createElement('button');
+        choiceButton.textContent = questions[index].choices[i];
+        choiceButton.addEventListener('click', function () {
+            handleAnswerClick(this.textContent);
+        });
+        questionContainer.appendChild(choiceButton);
+    }
 }
 
 // Function to handle user's answer choice
 function handleAnswerClick(choice) {
-  const correctAnswer = questions[currentQuestionIndex].answer;
-  if (choice === correctAnswer) {
-    // Handle correct answer
-    score++;
-    feedbackElement.textContent = 'Correct!';
-  } else {
-    // Handle incorrect answer
-    timer -= 15;
-    if (timer < 0) {
-      timer = 0;
+    const correctAnswer = questions[currentQuestionIndex].answer;
+    if (choice === correctAnswer) {
+        score++;
+        feedbackElement.textContent = 'Correct!';
+    } else {
+        timer -= 15;
+        if (timer < 0) {
+            timer = 0;
+        }
+        feedbackElement.textContent = 'Incorrect!';
     }
-    feedbackElement.textContent = 'Incorrect!';
-  }
 
-  // Move to the next question
-  currentQuestionIndex++;
+    currentQuestionIndex++;
 
-  if (currentQuestionIndex < questions.length) {
-    displayQuestion(currentQuestionIndex);
-  } else {
-    // Handle quiz completion
-    clearInterval(interval);
-    handleQuizCompletion();
-  }
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion(currentQuestionIndex);
+    } else {
+        clearInterval(interval);
+        handleQuizCompletion();
+    }
 }
 
 // Add an event listener to start the quiz when a button is clicked
-const startButton = document.getElementById('start-button'); // Add a button with this ID in your HTML
+const startButton = document.getElementById('start-button');
 startButton.addEventListener('click', startQuiz);
-
